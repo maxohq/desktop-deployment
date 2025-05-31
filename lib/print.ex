8904c8a69447ecd6c28348c0694da144fb8b405a
Roman Heinrich <roman.heinrich@gmail.com>
2025-05-31 13:50:18 +0200

    Chore: vendor in decorator package

---
+++ lib/print.ex
@@ -0,0 +1,10 @@
+defmodule Desktop.PrintDecorator do
+  use Desktop.Decorator.Define, print: 0
+
+  def print(body, context) do
+    quote do
+      IO.puts("Function called: " <> Atom.to_string(unquote(context.name)))
+      unquote(body)
+    end
+  end
+end
