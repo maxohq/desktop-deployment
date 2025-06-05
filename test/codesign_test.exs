cbdb2ba683cc6fe42a68718eb69c04fef8357ef4
Roman Heinrich <roman.heinrich@gmail.com>
2025-06-05 04:14:37 +0200

    Chore: adjust tests to use new modules

--- test/codesign_test.exs
+++ test/codesign_test.exs
@@ -3,7 +3,7 @@ defmodule CodesignTest do
 
   test "codesign a new binary" do
     {_, 0} = System.cmd("gcc", ["test/priv/main.c", "-o", "unsigned_main"])
-    DesktopDeployment.Package.MacOS.codesign_executable("unsigned_main")
+    DesktopDeployment.Os.Macos.MakeRelease.codesign_executable("unsigned_main")
     File.rm("unsigned_main")
   end
 end
